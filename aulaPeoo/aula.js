69d279e196197695157936fcc89d0159f78f1fab
Gustavo Andrey <Gustavo.Andrey@escolar.ifrn.edu.br>
2024-03-11 07:36:58 -0300

    Organização de arquivos

---
+++ aulaPeoo/aula.js
@@ -0,0 +1,30 @@
+let msg1 = "String com aspas duplas";
+let msg2 = 'String com aspas simples';
+let msg3 = `String com 
+       crase, permite múltiplas 
+       linhas`;
+
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
+
+const nome = 'Fulano';
+const idade = 15;
+
+console.log(nome + ' tem ' + idade + ' anos de idade.');
+console.log(`${nome} tem ${idade} anos de idade.`);
+
+let mensagem = "Mensagem";
+let nota = 9.5;
+console.log(typeof(mensagem));
+console.log(typeof(idade));
+console.log(typeof(nota));
+
+console.log(5 == 5);
+console.log(5 === 5);
+console.log(5 == '5');
+console.log(5 === '5');
+
+console.log(true && false);
+console.log(1 && 0);
+console.log(1 || 0);
