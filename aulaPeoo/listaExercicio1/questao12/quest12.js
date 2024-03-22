6d96725d876b0efc08ef70c5d41655965a5e3f8e
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-03-22 11:28:45 +0000

    questao12

--- aulaPeoo/listaExercicio1/questao12/quest12.js
+++ aulaPeoo/listaExercicio1/questao12/quest12.js
@@ -1,9 +1,11 @@
-function cresce(){
+function calc(){
     let list = document.querySelector("#list").value
     list = list.split(" ")
-    list = list.sort()
-    list = list.reverse()
-    console.log(list)
+    let numeros = list.map(Number)
+    let total = 0
+    for (let i = 0; i < numeros.length; i++ ){
+        total += numeros[i];
+    }
 
-    document.getElementById('result').innerHTML = `${list[0]}`
+        document.getElementById('result').innerHTML = `${total}`
 }
