a48ed079874dc811f3fcae9feb006e1567d9d848
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-07-05 11:27:07 +0000

    voltadepeoo

--- aulaPeoo/listaExercicio1/questao20/quest20.js
+++ aulaPeoo/listaExercicio1/questao20/quest20.js
@@ -1,17 +1,11 @@
-
-
-
-
-
-
-
-
-
 function vremail(){
+    document.getElementById("emailInvalido").innerHTML = ``
     let email = document.querySelector("#useremail").value;
-    console.log(email)
-    if (email.search("@") != 1 && email.search(".") != 1){
+    if (email.search("@") == -1 || email.search(".") == -1){
         document.getElementById("emailInvalido").innerHTML = `Email inserido invalido`
+        return false
+    } else{
+        return true
     }
 }
 
@@ -20,11 +14,24 @@ function vrsenha(){
     let vrsenha = document.querySelector("#userpasswordconfirm").value
 
     let regex = /^(?=.*\d).{8,}$/
-    console.log(regex.test(senha))
-   if (senha.length <= 8){
-
-    document.getElementById('result').innerHTML = "sim"
-   } else {
-    document.getElementById('result').innerHTML = "nao"
-   }
+    if (senha === vrsenha && regex.test(senha) == true){
+        return true
+    } else {
+        return false
+    }
+}
+function vrcamps(){
+    let nome = document.querySelector("#username").value
+    console.log(nome)
+    //escrever um codigo para verificar se a string de nome, email e senha estao vazios.
+    //opcoes; teste de regex, metodo search ou tentar um if com string vazia.
+}
+function cheque(){
+    vrcamps()
+    /* let vremail1 = vremail()
+    let vrsenha1 = vrsenha()
+    let vrcamps1 = vrcamps()
+    if (vremail1 == true && vrsenha1 == true && vrcamps1 == true){
+        document.querySelector("#result").innerHTML = `SUCESSO`
+    } */
 }
