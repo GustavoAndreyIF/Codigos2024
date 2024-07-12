aadcaf8529513ef24f62ea5717905d0eb6783689
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-07-12 11:32:02 +0000

    mais uma vez na 21

--- aulaPeoo/listaExercicio1/questao21/quest21.js
+++ aulaPeoo/listaExercicio1/questao21/quest21.js
@@ -1,8 +1,15 @@
 function outraOp(){
-    const select = document.getElementById("categoria").selectedIndex;
-    console.log(select);
-    if (select === 1){
-        let outro = document.body.createElement(input, {type="text" id="outrosl" placeholder="Escreva a outra opção"}) =
-        
+    const selecao = document.getElementById("categoria").selectedIndex;
+    const container = document.getElementById("container")
+
+    let outinput = document.createElement("input")
+    console.log(outinput)
+    outinput.setAttribute("type", "text")
+    outinput.setAttribute("id", "outinput")
+    console.log(outinput)
+    if(selecao == 1){
+        container.appendChild(outinput)
+    } else if(outinput){
+        container.removeChild(outinput)
     }
 }
