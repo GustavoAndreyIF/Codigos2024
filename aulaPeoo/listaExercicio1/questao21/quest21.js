b13f96c69bfa4bf871142fb4b615c1b728379015
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-07-16 11:31:25 +0000

    questao21

--- aulaPeoo/listaExercicio1/questao21/quest21.js
+++ aulaPeoo/listaExercicio1/questao21/quest21.js
@@ -16,3 +16,37 @@
 
 
 /*Construir script para adicionar valores dentro de uma tabela, pode usar innerhtml ou insertBefore*/
+
+function registrarNome(){
+    const nomeProduto = document.querySelector("#nome").value
+    if (nomeProduto != ""){
+    return nomeProduto
+    }
+}
+
+function registrarPreco(){
+    const precoProduto = document.querySelector("#preco").value
+    if (precoProduto != ""){
+    return precoProduto
+    }
+}
+
+function registrarCategoria(){
+    const categoriaProduto = document.querySelector("#categoria").value
+    return categoriaProduto
+}
+
+function registrarDescricao(){
+    const descricaoProduto = document.querySelector("#descricao").value
+    if (descricaoProduto != ""){
+    return descricaoProduto
+    }
+}
+
+function registrarTabela(){
+    const tabela = document.querySelector("#tabela").insertRow(-1)
+    tabela.insertCell(0).textContent = registrarNome()
+    tabela.insertCell(1).textContent = registrarPreco()
+    tabela.insertCell(2).textContent = registrarCategoria()
+    tabela.insertCell(3).textContent = registrarDescricao()
+}
