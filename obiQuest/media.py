3476bbb69ee5ce32e47375ae3ebe9e2b51f3259e
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-07-31 16:54:56 +0000

    mais obi

--- obiQuest/media.py
+++ obiQuest/media.py
@@ -1,8 +1,19 @@
-a, b = map(int, input())
+a, b = map(int, input().split())
 
-def media(a, b):
-    #media
-    booResp = True
-    c = b
-    while(True):
-        
+def mediaOuMediana(a, b):
+    def media(a1, b1, c1):
+        valorMedia = (a1+b1+c1)/3
+        return valorMedia
+    def mediana(a2, b2, c2):
+        valorMediana = [a2, b2, c2]
+        valorMediana.sort()
+        return valorMediana[1]
+    c = 0
+    while(c < b):
+        c += 1
+        if(media(a, b, c) == mediana(a, b, c)):
+            break
+    return c
+if(1 <= a <= b <= 10**9):
+    resultado = mediaOuMediana(a, b)
+    print(resultado)
---
+++ obiQuest/poligrama.py
@@ -0,0 +1,19 @@
+#nLetras = int(input())
+#palavra = str(input())
+
+def poligrama(n, p):
+    metade1 = list(p[:n//2])
+    metade2 = list(p[n//2:])
+    metade = list(p[:n//2])
+    verdade = -1
+    for i in range(n):
+        if(p[i] == p[0]):
+            verdade += 1
+    if(verdade > n//2):
+        return p[0]
+    if(metade1.sort() == metade2.sort() and metade1[0] == metade2[0]):
+        return "".join(metade)
+    return "*"
+
+resultado = poligrama(5, "xxxxx")
+print(resultado)
