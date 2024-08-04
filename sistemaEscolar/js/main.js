c859037b6f1f5d4cd622dd41a781e265e6bdd7fc
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-08-04 16:59:01 +0000

    mais do cadastrar aluno

--- sistemaEscolar/js/main.js
+++ sistemaEscolar/js/main.js
@@ -1,9 +1,10 @@
 class SistemaEscolar {
     constructor(){
         this.estudantes = JSON.parse(localStorage.getItem("estudantes")) || []
-        function salvarLocal(){
-            localStorage.setItem("estudantes", JSON.stringify(this.estudantes))
-        }
+        
+    }
+    salvarLocal(){
+        localStorage.setItem("estudantes", JSON.stringify(this.estudantes))
     }
     cadastrar(matricula, nome, idade){
         let estudante = {
@@ -48,3 +49,38 @@ class SistemaEscolar {
 
 
 const sistema = new SistemaEscolar()
+
+function cadastrarAluno(){
+    const nome = document.querySelector("#nome").value
+    const idade = document.querySelector("#idade").value
+    const matricula = document.querySelector("#matricula").value
+
+    sistema.cadastrar(matricula, nome, idade)
+    exibirEstudantes()
+}
+
+function exibirEstudantes(){
+    const estudantes = sistema.listar()
+    const tabela = document.querySelector("#tabelaEstudantes tbody")
+    
+    tabela.innerHTML = ""
+    
+    estudantes.forEach(estudante => {
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+        <td><input type="checkbox"></td>
+        <td>${estudante.nome}</td>
+        <td>${estudante.idade}</td>
+        <td>${estudante.matricula}</td>
+        `
+        tabela.appendChild(linha)     
+    });
+
+}
+
+function selecionarTodos(checar){
+    const caixas = document.querySelectorAll(`input[type="checkbox"]`)
+    caixas.forEach(checkbox => checkbox.checked = checar.checked)
+}
+
+    document.addEventListener("DOMContentLoaded", exibirEstudantes())
