ef6c897d4dcdc01bf89236cb2afe06de93fb8b82
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-08-16 11:27:51 +0000

    async function de URLs de APIs

--- aulaPeoo/listaJSON/questJSON01/questJSON01.js
+++ aulaPeoo/listaJSON/questJSON01/questJSON01.js
@@ -1,12 +1,34 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(Response => Response.json())
-    .then(dados => {
-        localStorage.setItem("posts", JSON.stringify(dados))
-    })
-let posts = JSON.parse(localStorage.getItem("posts")) || []
+// lista de URLs das APIs
+const urls = [
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/comments",
+    "https://jsonplaceholder.typicode.com/photos",
+    "https://jsonplaceholder.typicode.com/users"
+]
 
+// Função para chamar todas as APIs e armazerna no LocalStorage
+async function fetchData(urls) {
+    try {
+        for(i=0; i < urls.length; i++){
+            let reposta = await fetch(urls[i])
+            // esse await e pra esperar a reposta da promise
+            let dados = await reposta.json()
+            let index = `apiDados${i}`
+            // o index e o valor para acessar cada api diferente dentro do getItem
+            localStorage.setItem(index, JSON.stringify(dados))
+        }
+        console.log("Armazenado com sucesso")
+    }
+    catch(error) {
+        console.error("erro na busca do dado:", error)
+    }
+}
+
+fetchData(urls)
 
 function exibirPosts(){
+    let dataPosts = localStorage.getItem("apiDados1")
+    let posts = JSON.parse(dataPosts)
     const divposts = document.querySelector("#posts")
     divposts.innerHTML = ""
 
