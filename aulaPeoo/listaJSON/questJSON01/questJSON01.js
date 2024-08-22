bb462598672268d8744eaf5c3202b73b1e3c4013
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-08-22 11:22:07 +0000

    mais JSON

--- aulaPeoo/listaJSON/questJSON01/questJSON01.js
+++ aulaPeoo/listaJSON/questJSON01/questJSON01.js
@@ -1,4 +1,3 @@
-// lista de URLs das APIs
 const urls = [
     "https://jsonplaceholder.typicode.com/posts",
     "https://jsonplaceholder.typicode.com/comments",
@@ -6,25 +5,29 @@ const urls = [
     "https://jsonplaceholder.typicode.com/users"
 ]
 
-// Função para chamar todas as APIs e armazerna no LocalStorage
 async function fetchData(urls) {
     try {
-        for(i=0; i < urls.length; i++){
-            let reposta = await fetch(urls[i])
-            // esse await e pra esperar a reposta da promise
-            let dados = await reposta.json()
+        for (let i = 0; i < urls.length; i++) {
+            let resposta = await fetch(urls[i])
+            let dados = await resposta.json()
             let index = `apiDados${i}`
-            // o index e o valor para acessar cada api diferente dentro do getItem
             localStorage.setItem(index, JSON.stringify(dados))
         }
         console.log("Armazenado com sucesso")
-    }
-    catch(error) {
-        console.error("erro na busca do dado:", error)
+    } catch (error) {
+        console.error("Erro na busca do dado:", error)
     }
 }
 
-async function exibirPosts(){
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+async function exibirPosts() {
     await fetchData(urls)
     let dataPosts = localStorage.getItem("apiDados0")
     let dataUsers = localStorage.getItem("apiDados3")
@@ -32,26 +35,47 @@ async function exibirPosts(){
     let posts = JSON.parse(dataPosts)
     let users = JSON.parse(dataUsers)
     let photos = JSON.parse(dataPhotos)
+
+    const userMap = new Map(users.map(user => [user.id, user]))
+    const photosMap = new Map(photos.map(photo => [photo.albumId, photo]))
+
+    shuffleArray(posts);
+
     const divposts = document.querySelector("#posts")
     divposts.innerHTML = ""
 
-    posts.forEach(dadospost => {
-        const topico = document.createElement("div")
-        topico.innerHTML = `
-        <div id="${dadospost.id} style="padding: 10px 0px;">
-            <div id="userIcon" style="display: flex; align-items: center;">
-                <img src="${photos[dadospost.id - 1]["url"]}" style="width: 50px; border-radius: 50%; margin: 0px 5px 0px 0px">
-                <div>
-                    <p style="margin: 0 0 0 0; font-style: bold:"><b>${users[dadospost.id - 1]["name"]}</b></p>
-                    <p style="margin: 0 0 0 0;">${users[dadospost.id - 1]["username"]}</p>
+    if (Array.isArray(posts) && Array.isArray(users) && Array.isArray(photos)) {
+        posts.forEach(dadospost => {
+            const user = userMap.get(dadospost.userId)
+            const photo = photosMap.get(dadospost.userId)
+
+            const topico = document.createElement("div")
+            topico.innerHTML = `
+            <div id="${dadospost.id}" style="padding: 10px 0px;">
+                <div id="userIcon" style="display: flex; align-items: center;">
+                    <img src="${photo ? photo.url : "default-image.png"}" style="width: 50px; border-radius: 50%; margin: 0px 5px 0px 0px">
+                    <div>
+                        <p style="margin: 0 0 0 0; font-style: bold;"><b>${user ? user.name : "Unknown User"}</b></p>
+                        <p style="margin: 0 0 0 0;">${user ? user.username : "Unknown Username"}</p>
+                    </div>
                 </div>
-            </div>
-            <p style="font-size: 24px ;padding: 10px 0px 0px 0px; margin: 0 0 0 0"><b>${dadospost.title}</b></p>
-            <p style="font-size: 18px ;padding: 5px 0px 0px 0px; margin: 0 0 0 0">${dadospost.body}</p>
-        </div><br>  
-        `
-        divposts.appendChild(topico)
+                <p style="font-size: 24px ;padding: 10px 0px 0px 0px; margin: 0 0 0 0"><b>${dadospost.title}</b></p>
+                <p style="font-size: 18px ;padding: 5px 0px 0px 0px; margin: 0 0 0 0">${dadospost.body}</p>
+            </div><br>
+            `
+            divposts.appendChild(topico)
+        })
     }
-);
 }
+
+async function exibirComentarios() {
+    await fetchData(urls)
+    let dataComments = localStorage.getItem("apiDados1")
+    let comments = JSON.parse(dataComments)
+
+    comments.forEach(dadoscomment => {
+        
+    })
+}
+
 exibirPosts()
