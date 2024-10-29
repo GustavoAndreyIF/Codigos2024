b9a7884ced4301df5e5e3fbc4007fd011b7f6eab
Gustavo Andrey <gustavo.andrey@escolar.ifrn.edu.br>
2024-10-29 11:47:53 -0300

    correcao testeTS

--- testes/testeTS.ts
+++ testes/testeTS.ts
@@ -7,14 +7,14 @@ class UserAccount {
     name: string | null;
     id: number;
    
-    constructor(name: string, id: number) {
+    constructor(name: string | null, id: number) {
       this.name = name;
       this.id = id;
     }
   }
 
-const namePrompt = prompt("Enter your name: ")
-const idPrompt = prompt("Enter your ID: ")
+const namePrompt = prompt("Enter your name: ") ?? ""
+const idPrompt = prompt("Enter your ID: ") ?? "0"
 
 const user: User = new UserAccount(namePrompt, parseInt(idPrompt));
 console.log(user)
