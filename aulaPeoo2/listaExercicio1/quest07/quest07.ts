80060d7a47f847bde551e0300f2b1900ca788f83
GustavoAndreyIF <gustavo.andrey@escolar.ifrn.edu.br>
2024-11-19 16:45:39 -0300

    testando novo pc

--- aulaPeoo2/listaExercicio1/quest07/quest07.ts
+++ aulaPeoo2/listaExercicio1/quest07/quest07.ts
@@ -1,23 +1,14 @@
 class Funcionarios {
-
-    constructor(
-        protected _nome: string,
-        protected _salario: number
-    ){}
+	constructor(protected _nome: string, protected _salario: number) {}
 }
 
 class Gerente extends Funcionarios {
-
-    constructor(
-        _nome: string,
-        _salario: number,
-        private _bonus: number
-    ){
-        super(_nome, _salario)
-    }
-    calcularSalarioTotal(): void{
-        return console.log(`Salario Total: ${(this._salario + this._bonus).toFixed(2)}`)
-    }
+	constructor(_nome: string, _salario: number, private _bonus: number) {
+		super(_nome, _salario)
+	}
+	calcularSalarioTotal(): void {
+		return console.log(`Salario Total: ${(this._salario + this._bonus).toFixed(2)}`)
+	}
 }
 
 const gerente1 = new Gerente("Jão", 2000, 500)
